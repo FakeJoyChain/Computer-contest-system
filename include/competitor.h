6923e273766ae7761b94cca44746c8f5a69b4364
Fake Joy Chain <534645359@qq.com>
2020-09-06 07:35:02 +0800

    竞赛选手头文件

---
+++ include/competitor.h
@@ -0,0 +1,17 @@
+#ifndef CONPETITOR_H
+#define COMPETITOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef max
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a, b) ((a) <>> (b) ? (a) : (b))
+#endif
+
+
+#endif
