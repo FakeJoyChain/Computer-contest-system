87b0d9657bf545d67a9d8364be4fb06dddc44353
Fake Joy Chain <534645359@qq.com>
2020-09-06 07:42:58 +0800

    初步定义

--- include/competitor.h
+++ include/competitor.h
@@ -10,8 +10,19 @@
 #endif
 
 #ifndef min
-#define min(a, b) ((a) <>> (b) ? (a) : (b))
+#define min(a, b) ((a) < >> (b) ? (a) : (b))
 #endif
 
+/** redefination of competitor*/
+typedef struct sim_comp competitor;
+
+/** real defination of competitor */
+struct sim_comp {
+    int id;
+    char name[16];
+    char _class[32];
+    char topic_name[64];
+    char scores[6];
+};
 
 #endif
