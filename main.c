06cda4c9bfee34cb4e5ebaa9ca4a27e3e719db7a
Fake Joy Chain <534645359@qq.com>
2020-09-10 10:15:46 +0800

    略作修改

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+#define AUTO_APPEND
+
 #include "competitor.h"
 
 int main(int argc, char* argv[]) {
-    // competitors competitor = get_competitor();
+    //competitors competitor = get_competitor();
     competitors* cptr;
     int n = read_competitors(&cptr);
     printf("%d, %p\n", n, cptr);
+    append_competitor();
     return 0;
 }
